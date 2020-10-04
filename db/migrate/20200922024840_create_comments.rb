a8b3dca35f411f63bb2354465419a0e4d3810d8a
Iván García <ivan@pentadigitales.com>
2020-10-04 18:13:52 -0500

    This is the commit for second task

--- db/migrate/20200922024840_create_comments.rb
+++ db/migrate/20200922024840_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.references :post, null: false, foreign_key: true
       t.text :body
-
       t.timestamps
     end
   end
